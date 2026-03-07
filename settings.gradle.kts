fa9a1c6d002f4ee259a2fc5d362ace7a51be5cec
malaopr <154706813+malaopr@users.noreply.github.com>
2026-03-07 19:42:51 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,18 @@
  * The settings file is used to specify which projects to include in your build.
  * For more detailed information on multi-project builds, please refer to https://docs.gradle.org/8.9/userguide/multi_project_builds.html in the Gradle documentation.
  */
-
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    plugins {
+        kotlin("jvm") version "1.9.25"
+        kotlin("plugin.spring") version "1.9.25"
+        id("org.springframework.boot") version "3.5.9"
+        id("io.spring.dependency-management") version "1.1.7"
+    }
+}
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
